0fdeada20c9751277f1a191e65a54f8217463cc5
InterfazHombreMaquina <clgr@uma.es>
2025-05-07 19:00:59 +0200

    Update forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,12 +4,14 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Op
 from app.models import User
 from flask_login import current_user
 
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     organization = StringField('Organization', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=8, message="Password must be at least 8 characters.")
+    ])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
@@ -24,13 +26,11 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     rememberMe = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
 
 class ResetPasswordRequestForm(FlaskForm):
     """Form for users to request a password reset."""
@@ -39,14 +39,20 @@ class ResetPasswordRequestForm(FlaskForm):
 
 class ResetPasswordForm(FlaskForm):
     """Form for users to reset their password."""
-    password = PasswordField('New Password', validators=[DataRequired()])
+    password = PasswordField('New Password', validators=[
+        DataRequired(),
+        Length(min=8, message="Password must be at least 8 characters.")
+    ])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
 
 class UpdateProfileForm(FlaskForm):
     email = StringField('New Email', validators=[DataRequired(message="Email cannot be empty."), Email()])
 
-    password = PasswordField('New Password', validators=[Optional(), Length(min=4, message="Password must be at least 6 characters.")])
+    password = PasswordField('New Password', validators=[
+        Optional(),
+        Length(min=8, message="Password must be at least 8 characters.")
+    ])
     confirm_password = PasswordField('Confirm Password', validators=[Optional(), EqualTo('password', message="Passwords must match.")])
 
     current_password = PasswordField('Current Password', validators=[DataRequired(message="You must enter your current password.")])
