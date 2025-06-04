a2635b783274bd4872c7a90a92113f2964460ab3
InterfazHombreMaquina <clgr@uma.es>
2025-06-04 09:57:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY', '__REPLACEWITHSECRETKEY__')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','sqlite:///' + os.path.join(basedir, 'app.db'))
+    SECRET_KEY = os.getenv('SECRET_KEY') or '__REPLACEWITHSECRETKEY__'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'localhost')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 8025))
+    MAIL_SERVER = os.getenv('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.getenv('MAIL_PORT') or 8025)
     UPLOAD_FOLDER = 'uploads'
