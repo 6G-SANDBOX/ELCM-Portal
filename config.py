b0e3e9743c689a1c5694db77b515763974e27037
Cristóbal López de Gamarra Recio <clgr@uma.es>
2025-06-05 08:27:29 +0000

    Edit config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv(dotenv_path='./.flaskenv', verbose=True)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '__REPLACEWITHSECRETKEY__'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.getenv('SECRET_KEY') or '__REPLACEWITHSECRETKEY__'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
+    MAIL_SERVER = os.getenv('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.getenv('MAIL_PORT') or 8025)
     UPLOAD_FOLDER = 'uploads'
